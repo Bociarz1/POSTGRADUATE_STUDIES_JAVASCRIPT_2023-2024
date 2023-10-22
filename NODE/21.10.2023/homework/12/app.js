a7ddb221f560ad7f252fc7dbddaa31b2cb05c2e5
Bociarz1 <mikrofoncrk@gmail.com>
2023-10-22 16:46:52 +0200

    fix task 12  NODE/21.10.2023/homework

--- NODE/21.10.2023/homework/12/app.js
+++ NODE/21.10.2023/homework/12/app.js
@@ -1,7 +1,7 @@
 // 12. Aplikacja złożona jest z jednego pliku: app.js. Podczas uruchamiania możemy przekazać jej dodatkowy parametr który jest ścieżką do istniejącego na dysku folderu. Zadaniem aplikacji jest wyświetlanie w konsoli nazw wszystkich plików i folderów znajdujących się bezpośrednio we wskazanym folderze. Uruchomienie z niewłaściwą liczbą parametrów powinno skutkować wyświetleniem komunikatu w konsoli. Należy wykorzystać moduł Core'owy File system (https://nodejs.org/dist/latest-v18.x/docs/api/fs.html , potrzebna funkcja przyrostek Sync)
 
 const { readdirSync } = require('fs');
-const { param = process.argv } = require('process');
+const param = process.argv;
 
 if (param.length > 3) {
   console.log('zbyt dużo parametrów!');
